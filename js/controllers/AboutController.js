e74a83558618eed6cf81a5f7614f77dc24b4c4e0
Mauya <smkadungure@live.co.uk>
2017-08-25 17:59:59 +0100

    Make tabs links

--- js/controllers/AboutController.js
+++ js/controllers/AboutController.js
@@ -1,10 +1,10 @@
-	angular.module('RouteControllers', [])
-	.controller('AboutController', ['$scope', function($scope) {
+angular.module('RouteControllers', [])
+app.controller('AboutController', ['$scope', function($scope) {
     $scope.tabContent = [
     	{
     		name: 'Kelly Rowland',
     		photo: 'imgs/about/bioK1.jpg',
-    		bio: "Kelly Rowland rose to fame with the success of Destiny's Child during the late '90s. During that time, Destiny's Child catapulted into the new millennium as one of the best-selling female musical groups in history. Rowland, who was barely in her twenties, had been imagining such stardom for a long time. Born and raised in Atlanta, Gerogia, Rowland began singing as soon as she heard Whitney Houston. She was only four at the time, but Houston's 1985 self-titled debut was enough to motivate the little girl. Performances with the church choir followed, but Rowland's developing interest in music blossomed when she and her family moved to Houston, Texas. She befriended another inspiring singer, Beyoncé Knowles, and the two became fast friends. In the mid-'90s, the two girls formed Destiny's Child. Destiny's Child made an impressive debut in 1997 with first single "No No No"; their self-titled full-length followed the next winter and went platinum before the year's end. Two more albums followed -- The Writing's on the Wall (1999) and Survivor (2001) -- earning the group countless fans across the globe as well as millions of records sold."
+    		bio: "Kelly Rowland rose to fame with the success of Destiny's Child during the late '90s. During that time, Destiny's Child catapulted into the new millennium as one of the best-selling female musical groups in history. Rowland, who was barely in her twenties, had been imagining such stardom for a long time. Born and raised in Atlanta, Gerogia, Rowland began singing as soon as she heard Whitney Houston. She was only four at the time, but Houston's 1985 self-titled debut was enough to motivate the little girl. Performances with the church choir followed, but Rowland's developing interest in music blossomed when she and her family moved to Houston, Texas. She befriended another inspiring singer, Beyoncé Knowles, and the two became fast friends. In the mid-'90s, the two girls formed Destiny's Child. Destiny's Child made an impressive debut in 1997 with first single 'No No No'; their self-titled full-length followed the next winter and went platinum before the year's end. Two more albums followed -- The Writing's on the Wall (1999) and Survivor (2001) -- earning the group countless fans across the globe as well as millions of records sold."
     	},
     	{
     		name: 'Beyonce Knowles',
@@ -14,15 +14,7 @@
     	{
     		name: 'Michelle Williams',
     		photo: 'imgs/about/bioK3.jpg',
-    		bio: "Born in 1980, Michelle Williams started singing as a child. She performed gospel music for much of her early life. In 1999, Williams became a backup singer for R&B artist Monica. She then joined Destiny's Child the following year. With Destiny's Child, Williams made such hits as "Survivor" and "Bootylicious." The group broke up in 2005, but they have reunited several times since. Williams has released several solo albums as well, including 2008's Unexpected. She performed at the Super Bowl with Destiny's Child in 2013."
-    	},
-    	]
-
-    $scope.setTab = function(newTab){
-      $scope.tab = newTab;
-    };
-
-    $scope.isSet = function(tabNum){
-      return $scope.tab === tabNum;
-    };
+    		bio: "Born in 1980, Michelle Williams started singing as a child. She performed gospel music for much of her early life. In 1999, Williams became a backup singer for R&B artist Monica. She then joined Destiny's Child the following year. With Destiny's Child, Williams made such hits as 'Survivor' and 'Bootylicious.' The group broke up in 2005, but they have reunited several times since. Williams has released several solo albums as well, including 2008's Unexpected. She performed at the Super Bowl with Destiny's Child in 2013."
+    	}
+    	];
 }]);
--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,18 +1,18 @@
 angular.module('RouteControllers', [])
-    .controller('HomeController', function($scope) {
-        $scope.title = "Destiny's Child Collections!";
-        $scope.band = [ 
-        	{
-		    	img: 'imgs/home/himg6.jpg',
-		    	caption: 'Strong women survivor'
-		    },
-		    {
-		    	img: '/imgs/home/himg5.jpg',
-		    	caption: 'Something is ...'
-		    },
-		    {
-		    	img: '/imgs/home/himg7.jpg',
-		    	caption: 'You deserve to have fun!'
-		    },
-		]
-    });
+app.controller('HomeController', function($scope) {
+    $scope.title = "Destiny's Child Collections!";
+    $scope.band = [ 
+    	{
+	    	img: 'imgs/home/himg6.jpg',
+	    	caption: 'Survivor'
+	    },
+	    {
+	    	img: '/imgs/home/himg5.jpg',
+	    	caption: 'Something is ...'
+	    },
+	    {
+	    	img: '/imgs/home/himg7.jpg',
+	    	caption: 'Brown eyes'
+	    }
+	];
+});
